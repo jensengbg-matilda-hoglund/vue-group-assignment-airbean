4550ccef1d105c365b95934a581177dca8b622eb
Matilda Höglund <hoglund.matilda@gmail.com>
2020-03-24 11:46:45 +0100

    profile history-design ok

---
+++ src/components/register.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="register">
+    <img src="../assets/graphics/logo.svg" alt />
+    <h1 class="headline">Välkommen till AirBean-familjen!</h1>
+    <p>Genom att skapa ett konto nedan kan du spara och se din orderhistorik.</p>
+    <label for="name">Namn</label>
+    <input type="text" id="name" />
+    <label for="email">Epost</label>
+    <input type="text" id="email" />
+    <button class="radio" type="radio">GDPR Ok!</button>
+    <button class="btn">Brew me a cup!</button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "../assets/scss/variables.scss";
+
+.register {
+  display: flex;
+  flex-direction: column;
+  color: $black;
+  background: $latte;
+  width: 34rem;
+  height: 57rem;
+  border-radius: 3px;
+}
+</style>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,16 +1,239 @@
 <template>
-  <div class="order-history">
-    <h1>Order History goes here</h1>
-    <button @click="send">send order</button>
+  <div class="profile">
+    <Nav v-if="openNav" @closeNav="nav" class="nav-overlay" />
+
+    <section class="content">
+      <div class="header">
+        <button @click="nav" class="nav-btn">
+          <img src="../assets/graphics/navicon.svg" alt />
+        </button>
+      </div>
+      <Register v-if="displayRegister" class="register" />
+      <section class="main">
+        <section class="user">
+          <img src="../assets/graphics/profile.svg" alt class="profile-img" />
+          <h2 class="user-name">Sixten Kaffelövér</h2>
+          <p class="user-email">sixten.kaffelover@zocom.se</p>
+        </section>
+        <section class="history">
+          <h1>Orderhistorik</h1>
+          <li v-for="(order, index) in orders" :key="index" class="order-list">
+            <div class="left">
+              <p class="order-nr">#{{order.orderNr}}</p>
+              <p class="total">total delsumma</p>
+            </div>
+            <div class="right">
+              <p class="date">{{order.timeStamp}}</p>
+              <p class="totalsum">{{order.totalValue}} Kr</p>
+            </div>
+          </li>
+          <span class="divider"></span>
+          <div class="sum-order">
+            <p class="total-spend">Totalt spenderat</p>
+            <p class="sum">456 Kr</p>
+          </div>
+        </section>
+      </section>
+    </section>
   </div>
 </template>
 
 <script>
+import Nav from "../components/nav";
+import Register from "../components/register";
+
 export default {
+  components: {
+    Nav,
+    Register
+  },
+  data: () => {
+    return {
+      openNav: false,
+      displayRegister: true,
+      orders: [
+        {
+          orderNr: "aadfhf455gSA",
+          timeStamp: "24/03/20",
+          cart: "not used",
+          totalValue: "445"
+        },
+        {
+          orderNr: "aadfhf455gSA",
+          timeStamp: "24/03/20",
+          cart: "not used",
+          totalValue: "445"
+        },
+        {
+          orderNr: "aadfhf455gSA",
+          timeStamp: "24/03/20",
+          cart: "not used",
+          totalValue: "445"
+        }
+      ]
+    };
+  },
   methods: {
-    send() {
-      this.$store.dispatch("sendOrder");
+    nav() {
+      if (this.openNav === true) {
+        this.openNav = false;
+      } else {
+        this.openNav = true;
+      }
+    }
+  },
+  mounted() {
+    if (localStorage.getItem("uuid")) {
+      this.displayRegister = false;
     }
   }
 };
 </script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/variables.scss";
+
+.nav-overlay {
+  position: absolute;
+  height: 106rem;
+  background-color: $black;
+}
+
+.register {
+  position: absolute;
+
+  padding: 2rem;
+  bottom: 28%;
+}
+
+.profile {
+  background: $latte;
+  height: 66.7rem;
+  background-color: $black;
+  background: url("../assets/graphics/graphics-header.svg") top no-repeat;
+
+  .content {
+    padding: 1.6rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 100%;
+    width: 100%;
+
+    .header {
+      display: flex;
+      margin-bottom: 6rem;
+
+      .nav-btn {
+        height: 4.8rem;
+        width: 4.8rem;
+        border-radius: 100%;
+        border: none;
+        cursor: pointer;
+      }
+    }
+
+    .main {
+      display: flex;
+      flex-direction: column;
+      margin-bottom: auto;
+      font-family: $worksans;
+
+      .user {
+        display: flex;
+        flex-direction: column;
+        color: $white;
+        align-items: center;
+        margin-bottom: 6rem;
+
+        .user-name {
+          font-size: 2.4rem;
+          font-weight: 600;
+          font-family: $PT;
+          margin: 2rem 0 0.5rem 0;
+        }
+
+        .user-email {
+          font-size: 1.4rem;
+          opacity: 0.8;
+        }
+      }
+
+      .history {
+        display: flex;
+        flex-direction: column;
+
+        h1 {
+          font-size: 2.2rem;
+          font-weight: 600;
+          font-family: $PT;
+          color: $white;
+          align-self: flex-start;
+          margin: 1rem 0;
+        }
+
+        .order-list {
+          display: flex;
+          width: 100%;
+          justify-content: space-between;
+          margin: 1rem 0;
+
+          .left,
+          .right,
+          p {
+            display: flex;
+            flex-direction: column;
+
+            font-size: 1.4rem;
+            font-family: $worksans;
+            color: $white;
+            font-weight: 500;
+          }
+          .left {
+            align-items: flex-start;
+            .order-nr {
+              font-weight: 600;
+              text-transform: uppercase;
+              opacity: 0.7;
+            }
+
+            .total {
+              opacity: 0.5;
+            }
+          }
+
+          .right {
+            align-items: flex-end;
+            .date {
+              opacity: 0.7;
+            }
+
+            .totalsum {
+              opacity: 0.5;
+            }
+          }
+        }
+
+        .divider {
+          border-bottom: 1px solid $white;
+          opacity: 0.6;
+          width: 100%;
+        }
+        .sum-order {
+          display: flex;
+          justify-content: space-between;
+          margin: 1.5rem 0;
+
+          .total-spend,
+          .sum {
+            font-size: 1.4rem;
+            font-weight: 600;
+            color: $white;
+            opacity: 0.8;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
