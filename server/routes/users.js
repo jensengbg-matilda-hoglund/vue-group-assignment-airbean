7e20d170ae59d5d0790ed9383491e0842d90c02c
Matilda Höglund <hoglund.matilda@gmail.com>
2020-03-26 14:06:54 +0100

    fixed addUser

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,23 +3,23 @@ const router = new Router();
 const db = require("../modules/user-operations");
 
 router.get("/", (req, res) => {
-    res.send(db.getUsers())
+  res.send(db.getUsers());
 });
 
 router.get("/:uuid", (req, res) => {
-    res.send(db.findUser(req.params.uuid))
-})
+  res.send(db.findUser(req.params.uuid));
+});
 
 router.post("/", (req, res) => {
-    let body = req.body;
-    let user = {
-        uuid: body.uuid,
-        name: body.name,
-        email: body.email,
-        orders: []
-    };
-    db.addUser(req.body);
-    res.send(user);
+  let body = req.body;
+  let user = {
+    uuid: body.uuid,
+    name: body.name,
+    email: body.email,
+    orders: []
+  };
+  db.addUser(user);
+  res.send(user);
 });
 
 module.exports = router;
