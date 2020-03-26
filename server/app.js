4cc162dd608bdbff77ab602af67c89ed4f04463b
jensengbg-eliza-smirnova <elizasmir@gmail.com>
2020-03-26 13:11:39 +0100

    spara user i db

--- server/app.js
+++ server/app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const bodyParser = require ("body-parser");
 
 const beansRouter = require("./routes/beans");
-const PORT = 5000;
+const usersRouter = require("./routes/users");
 
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use("/api/beans", beansRouter);
+app.use("/api/users", usersRouter);
 
-app.listen(5000, () => console.log(`Server started on port ${PORT}`));
+app.listen(5000, () => console.log(`Server started on port 5000`));
