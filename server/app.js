fbb005e0238d69b3b1683dba74ef42261a214a1b
Matilda Höglund <hoglund.matilda@gmail.com>
2020-03-27 13:01:16 +0100

    fix

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ const bodyParser = require ("body-parser");
 
 const beansRouter = require("./routes/beans");
 const usersRouter = require("./routes/users");
+const ordersRouter = require("./routes/orders");
 
 app.use(cors());
 app.use(express.json());
@@ -12,5 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use("/api/beans", beansRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/orders", ordersRouter);
+
 
 app.listen(5000, () => console.log(`Server started on port 5000`));
