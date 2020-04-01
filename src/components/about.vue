67805fba6162d567904b4ce7e93c5498617d4804
Matilda Höglund <hoglund.matilda@gmail.com>
2020-04-01 10:35:56 +0200

    deleted about and cart duplicates

--- src/components/about.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="about">
-    <h1>This is ABOUT</h1>
-  </div>
-</template>
--- src/components/cart.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="cart">
-    <h1>This is CART</h1>
-  </div>
-</template>
